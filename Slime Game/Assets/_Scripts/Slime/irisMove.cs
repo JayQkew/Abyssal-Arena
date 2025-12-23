c206302e6e9bff327a460b37489ad75164215a45
Jay-Lee Shih <131269959+JayQkew@users.noreply.github.com>
2025-12-23 22:44:36 +0200

    new face and colours

--- Slime Game/Assets/_Scripts/Slime/irisMove.cs
+++ Slime Game/Assets/_Scripts/Slime/irisMove.cs
@@ -1,31 +1,25 @@
 using UnityEngine;
 
-public class irisMove : MonoBehaviour
+public class IrisMove : MonoBehaviour
 {
     private Vector2 _parent;
-    public Vector3 eyePosition;
+
     [HideInInspector] public InputHandler inputHandler;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    private void Start()
     {
-        _parent = transform.parent.parent.parent.localPosition;
-        //transform.parent.localPosition = eyePosition;
-        inputHandler = transform.parent.parent.parent.parent.GetComponent<InputHandler>();
-        
+        _parent = transform.parent.localPosition;
+        inputHandler = transform.parent.parent.GetComponent<InputHandler>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        eyeFollow(inputHandler.moveInput);
+        EyeFollow(inputHandler.moveInput);
     }
 
-    public void eyeFollow(Vector3 pos)
+    private void EyeFollow(Vector3 pos)
     {
-        
-        Vector2 direction = new Vector2(((pos.x- _parent.x))/5 , ((pos.y - _parent.y))/5); //15 -0.443f/15
+        Vector2 direction = new Vector2((pos.x - _parent.x) / 2, (pos.y - _parent.y) / 2); //15 -0.443f/15
         transform.localPosition = direction;
-        
     }
-    
 }
