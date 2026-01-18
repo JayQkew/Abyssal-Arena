519a842c87ab98fd54c12cde68d36c3e7fda22ff
Jay-Lee Shih <131269959+JayQkew@users.noreply.github.com>
2026-01-18 12:29:21 +0200

    Finished Game

--- Slime Game/Assets/_Scripts/BallGame/Goals.cs
+++ Slime Game/Assets/_Scripts/BallGame/Goals.cs
@@ -1,3 +1,4 @@
+using System;
 using AppStates;
 using UnityEngine;
 using UnityEngine.Serialization;
@@ -7,10 +8,18 @@ namespace BallGame
     public class Goals : MonoBehaviour
     {
         public int scoringTeam;
+        private AudioSource _audioSource;
 
         public bool canScore = true;
+
+        private void Awake()
+        {
+            _audioSource = GetComponent<AudioSource>();
+        }
+
         private void OnTriggerEnter2D(Collider2D other) {
             if (other.gameObject.CompareTag("Ball") && canScore) {
+                _audioSource.Play();
                 AppState.Instance.gameplayState.matchState.PlayerPoint(scoringTeam);
             }
         }
